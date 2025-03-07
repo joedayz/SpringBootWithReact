75f826085da5299e1d1ee54fa6314ef0276a509e
Jose Diaz <jose.diaz@joedayz.pe>
2025-03-06 19:54:32 -0500

    header completo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect,useRef, useState } from 'react'
 import './App.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { getContacts } from './api/ContactService';
@@ -6,7 +6,7 @@ import Header from './components/Header'
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-  
+  const modalRef = useRef();
   const [data, setData] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -21,6 +21,12 @@ function App() {
       toastError(error.message);
     }
   };
+
+  const toggleModal = show => show ? modalRef.current.showModal() : modalRef.current.close();
+  
+  useEffect(() => {
+    getAllContacts();
+  }, []);
 
   return (
     <>
