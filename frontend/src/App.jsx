c90c9cd4b8270a4ea9eb2760a474a59d85d1e2e2
Jose Diaz <jose.diaz@joedayz.pe>
2025-03-06 19:48:58 -0500

    GetContacts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { getContacts } from './api/ContactService';
 import Header from './components/Header'
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getAllContacts = async (page = 0, size = 10) => {
+    try {
+      setCurrentPage(page);
+      const { data } = await getContacts(page, size);
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      toastError(error.message);
+    }
+  };
 
   return (
     <>
-      <Header/>
+      <Header toggleModal={toggleModal} nbOfContacts={data.totalElements}/>
       <main>
 
       </main>
 
       {/* Modal */}
-
+      <ToastContainer />
     </>
   )
 }
